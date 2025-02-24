78b41b4b4968e1f28cfeb489709f5537b7656594
Premroyal3041 <119612013+Premroyal3041@users.noreply.github.com>
2025-02-24 16:50:06 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./cart.css";
+import React from "react";
+import ProductTable from "./cart_Main";
+
+function App() {
+  return (
+    <div>
+      <ProductTable />
+    </div>
+  );
+}
+
+export default App;
